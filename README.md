c79623b2826d9b178fc5e146da3e244b382be9ad
anshyadav <anshyadav4244@gmail.com>
2022-04-08 16:39:58 +0530

    programmatically ui designing in deleteMartialArtActivity class

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Martial-Arts-SQLite-
 ## SQLite with Android
+ - model 
+        - DatabaseHandler class `extends SQLiteOpenHelper`
+        - Data Storage Class 
+
+## Topic Covered
+
+- ***UI Designing Programmatically***
+- RadioGroup 
+- RadioButton 
+
--- app/src/main/java/com/example/martialartsclub/DeleteMartialArtActivity.java
+++ app/src/main/java/com/example/martialartsclub/DeleteMartialArtActivity.java
@@ -3,12 +3,97 @@ package com.example.martialartsclub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
-public class DeleteMartialArtActivity extends AppCompatActivity {
+import com.example.martialartsclub.model.DatabaseHandler;
+import com.example.martialartsclub.model.MartialArt;
+
+import java.util.ArrayList;
+
+public class DeleteMartialArtActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, View.OnClickListener {
+
+    private DatabaseHandler mDatabaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_martial_art);
+
+        mDatabaseHandler = new DatabaseHandler(this);
+
+        updateTheUserInterface();
+    }
+
+
+    /**
+     * Creating UI using java code
+      */
+    private void updateTheUserInterface() {
+        ArrayList<MartialArt> allMartialArtObject = mDatabaseHandler.returnAllMartialArtObject();
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        ScrollView scrollView = new ScrollView(this);
+        RadioGroup radioGroup = new RadioGroup(this) ;
+
+        for(MartialArt martialArt : allMartialArtObject) {
+            // assigning radiobutton to every martialart object
+            RadioButton currentRadioButton = new RadioButton(this) ;
+            currentRadioButton.setId(martialArt.getMartialArtID());
+            currentRadioButton.setText(martialArt.toString());
+
+            radioGroup.addView(radioGroup);
+        }
+
+        radioGroup.setOnCheckedChangeListener(this);
+
+        Button btnBack = new Button(this) ;
+        btnBack.setText("Go back");
+        btnBack.setOnClickListener(this);
+
+        // adding radioGroup to ScrollView
+        scrollView.addView(radioGroup);
+
+        /**
+         *  specifying width and height of the back button
+         *     ********* VERY IMP *************
+         */
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        buttonParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonParams.setMargins(0,0,0, 70);
+
+        // Adding button to the relative layout
+        relativeLayout.addView(btnBack, buttonParams);
+
+        ScrollView.LayoutParams scrollViewParams = new ScrollView.LayoutParams(
+                ScrollView.LayoutParams.MATCH_PARENT,
+                ScrollView.LayoutParams.MATCH_PARENT
+        );
+
+        relativeLayout.addView(scrollView, scrollViewParams);
+
+        // setting this content View
+        setContentView(relativeLayout );
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        mDatabaseHandler.deleteMartialArtTupleFromDatabaseByID(checkedId);
+        Toast.makeText(this, "Martial Arts object is deleted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+       this.finish();
     }
 }
